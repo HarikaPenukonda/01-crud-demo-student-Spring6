3d532e09ad15e7574c4ee1e78c5636a1d52ea55e
harika penukonda <penukondaharika@gmail.com>
2023-05-23 22:09:20 -0500

    Update CruddemoApplication.java

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,13 +1,25 @@
 package com.luv2code.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		// The CommandLineRunner is from the Spring Boot Framework, Executed after the Spring Beans have been loaded
+		// Java Lambda Expression
+		return runner -> {
+			System.out.println("Hello world");
+		};
+
 	}
 
 }
