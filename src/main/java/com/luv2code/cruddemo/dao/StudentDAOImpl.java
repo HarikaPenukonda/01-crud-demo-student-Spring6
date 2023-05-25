8bb7914a64b5321e438b931ea875dd06ced232c9
harika penukonda <penukondaharika@gmail.com>
2023-05-25 01:05:57 -0500

    Add implementation to the method findById

--- src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -2,6 +2,7 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +13,7 @@ public class StudentDAOImpl implements StudentDAO{
     private EntityManager entityManager;
 
     // inject entity manager using constructor injection
-
+   //@Autowired
     public StudentDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
@@ -23,4 +24,12 @@ public class StudentDAOImpl implements StudentDAO{
     public void save(Student theStudent) {
         entityManager.persist(theStudent); // saves the student
     }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class,id);
+    }
+
+
 }
+
